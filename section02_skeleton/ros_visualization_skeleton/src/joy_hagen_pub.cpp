77f8a5aa10a516c5c339f50c30413a28ea951d34
GPrathap <ggeesara@gmail.com>
2022-06-10 12:43:35 +0300

    adding server and client

--- section02_skeleton/ros_visualization_skeleton/src/joy_hagen_pub.cpp
+++ section02_skeleton/ros_visualization_skeleton/src/joy_hagen_pub.cpp
@@ -5,7 +5,7 @@
 #include <std_msgs/Int32.h>
 #include <geometry_msgs/Vector3.h>
 
-// #include <ros_visualization_skeleton/SetSpeed.h>
+#include <ros_visualization_skeleton/SetSpeed.h>
 
 int main( int argc, char **argv )
 {
@@ -14,13 +14,13 @@ int main( int argc, char **argv )
   ros::NodeHandle n;
   ros::Publisher pub_temp = n.advertise<std_msgs::Int32>( "/joy_hagen/temperature", 1000);
   ros::Publisher pub_imu = n.advertise<geometry_msgs::Vector3>( "/joy_hagen/imu", 1000);
-  // ros::ServiceClient srv_speed = n.serviceClient<ros_visualization_skeleton::SetSpeed>("d");
+  ros::ServiceClient srv_speed = n.serviceClient<ros_visualization_skeleton::SetSpeed>("/speed");
 
   // TODO define message types 
 
   std_msgs::Int32 temp_data;
   geometry_msgs::Vector3 imu_data;
-
+  ros_visualization_skeleton::SetSpeed speed_srv;
   int i = 0;
   ros::Rate rate(1);
   while( ros::ok() ) {
@@ -35,6 +35,15 @@ int main( int argc, char **argv )
     pub_temp.publish(temp_data);
     pub_imu.publish(imu_data); 
     // TODO call the service srv_speed and if it get executed, print previous_speed and new_speed
+    speed_srv.request.desired_speed = 0.45*i;
+    bool is_fine = srv_speed.call(speed_srv);
+    if(is_fine){
+      std::cout<< "service respose "<< speed_srv.response.new_speed << std::endl;
+      // ROS_INFO_STREAM("service respose " << srv.response.heartbeat);
+    } else {
+      // std::cout<< "Something wrong with the server"<< std::endl;
+      ROS_INFO("Something wrong with the server");
+    }
     ++i;
     ros::spinOnce();
     rate.sleep();
